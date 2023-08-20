2968318625508e2039abf7d7e51d85f546bf37aa
Benoît Pntier <46506005+benpntier@users.noreply.github.com>
2023-08-20 04:17:27 +0200

    About

---
+++ src/components/Collapse/index.jsx
@@ -0,0 +1,27 @@
+import './index.scss'
+import React, { useState } from 'react'
+
+function Collapse({ title, text }) {
+  const [isOpen, setIsOpen] = useState(false)
+
+  //fonction pour gérer l'afichage du contenu des collapses
+  function open() {
+    setIsOpen(!isOpen)
+  }
+
+  return (
+    <div className="collapse">
+      <div className="collapse__title">
+        <h2 className="collapse__title__content">{title}</h2>
+        <div onClick={open} className="collapse__icon">
+          <i className={'fa-solid fa-chevron-' + (isOpen ? 'up' : 'down')}></i>
+        </div>
+      </div>
+      <div className="collapse__text">
+        {isOpen && <p className="collapse__text__content">{text}</p>}
+      </div>
+    </div>
+  )
+}
+
+export default Collapse
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,11 +1,23 @@
 import Banner from '../../components/Banner'
 import aboutBannerImage from '../../assets/banner-about.png'
+import aboutContent from '../../data/about.json'
+import Collapse from '../../components/Collapse'
+import './index.scss'
 
 function About() {
   document.title = 'Kasa - A Propos'
   return (
-    <main class="about max-width">
+    <main className="about max-width">
       <Banner img={aboutBannerImage} />
+      <div className="about__dropdown">
+        {aboutContent.map((paragraph) => {
+          return (
+            <div key={paragraph.id}>
+              <Collapse title={paragraph.title} text={paragraph.text} />
+            </div>
+          )
+        })}
+      </div>
     </main>
   )
 }
