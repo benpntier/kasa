635f24f0c3012705644c3a0f8b8ef9f1a8753512
Benoît Pntier <46506005+benpntier@users.noreply.github.com>
2023-08-21 00:42:05 +0200

    minor CSS fix

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -2,27 +2,22 @@ import React, { useState } from 'react'
 import './index.scss'
 
 function Carousel({ pictures }) {
-  const [visibleIndex, setVisibleIndex] = useState(0) // index of  the visible image
-  const nbPictures = pictures.length // total number of pictures
+  const [visibleIndex, setVisibleIndex] = useState(0)
+  const nbPictures = pictures.length
 
-  // display the previous picture (call on first picture displays last picture)
   const prevPicture = () => {
     setVisibleIndex(visibleIndex === 0 ? nbPictures - 1 : visibleIndex - 1)
   }
-
-  // display the next picture (call on last picture displays first picture)
   const nextPicture = () => {
     setVisibleIndex(visibleIndex === nbPictures - 1 ? 0 : visibleIndex + 1)
   }
 
-  // if there are no pictures
   if (!Array.isArray(pictures) || nbPictures <= 0) {
     return null
   }
 
   return (
     <section className="carousel">
-      {/* left arrow (if more than one picture) */}
       {nbPictures > 1 && (
         <div
           onClick={prevPicture}
@@ -32,7 +27,6 @@ function Carousel({ pictures }) {
         </div>
       )}
 
-      {/* right arrow (if more than one picture) */}
       {nbPictures > 1 && (
         <div
           onClick={nextPicture}
@@ -42,7 +36,6 @@ function Carousel({ pictures }) {
         </div>
       )}
 
-      {/* image */}
       {pictures.map((picture, i) => {
         return (
           <div key={i}>
@@ -57,7 +50,6 @@ function Carousel({ pictures }) {
         )
       })}
 
-      {/* number of picture */}
       {nbPictures > 1 && (
         <p className="carousel__number">
           {visibleIndex + 1}/{nbPictures}
