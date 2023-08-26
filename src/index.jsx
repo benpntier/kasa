03206e6d8f4259909bd78acca4430bb3ba0f8619
Benoît Pntier <46506005+benpntier@users.noreply.github.com>
2023-08-26 10:54:50 +0200

    ReactDOM.render => createRoot

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
@@ -8,8 +8,10 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Error from './components/Error'
 import './style/main.scss'
+const container = document.getElementById('root')
+const root = createRoot(container)
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <Router>
       <Header />
@@ -21,6 +23,5 @@ ReactDOM.render(
       </Routes>
       <Footer />
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
